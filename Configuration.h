6c14aa724cf5d02ef64c47161656a27ba6d9ca32
Stefanov <RStefanov@luxoft.com>
2018-03-15 11:44:59 +0200

    Decrease Z feedrate and acceleration

--- Configuration.h
+++ Configuration.h
@@ -589,16 +589,16 @@ const bool Z_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the logic
 // default settings
 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 4000, 500}  // 280 pignon emaritimmodels
-#define DEFAULT_MAX_FEEDRATE          {100, 100, 10, 25}    // (mm/sec)
+#define DEFAULT_MAX_FEEDRATE          {100, 100, 2, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {1000, 1000, 20, 10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
-#define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration in mm/s^2 for printing moves
+#define DEFAULT_ACCELERATION          450  // X, Y, Z and E acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000   // E acceleration in mm/s^2 for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1500    // X, Y, Z acceleration in mm/s^2 for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   450  // X, Y, Z acceleration in mm/s^2 for travel (non printing) moves
 
 // The speed change that does not require acceleration (i.e. the software might assume it can be done instantaneously)
 #define DEFAULT_XYJERK                10.0    // (mm/sec)
-#define DEFAULT_ZJERK                 10.0     // (mm/sec)
+#define DEFAULT_ZJERK                 0.0     // (mm/sec)
 #define DEFAULT_EJERK                 5.0    // (mm/sec)
 
 
